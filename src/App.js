eb4f22b268572f7fa0bc55fcc835a9b854f0d6c7
clebsonf <clebson.augusto@dcx.ufpb.br>
2022-10-21 12:46:58 -0300

    chore: add base project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from "react";
+import placeholderImg from "./images/undraw_relaxing_walk.svg";
+
+function App() {
+
+  return (
+    <div className="d-flex justify-content-center flex-column text-center">
+      <header>
+        <h1 className="mt-4 mb-5">Doggy Directory 🐶</h1>
+      </header>
+      <main role="main">
+    
+      </main>
+    </div>
+  );
+}
+
+export default App;
