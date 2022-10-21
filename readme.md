5a67045b0459741cd1ab91ce029beadcec3fb127
Clebsonf <clebson.augusto@dcx.ufpb.br>
2022-10-21 17:01:27 +0000

    feat: list all breeds

---
+++ readme.md
@@ -0,0 +1,8 @@
+# Requisitos
+
+## API url:
+https://dog.ceo/api/breed/{dog_breed}/images
+
+### Mostrar quantidade de fotos
+### Enquanto não clicar em buscar mostrar a imagem | images == 0
+### Loading | Hook para loading
--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { useEffect, useState } from "react";
 import placeholderImg from "./images/undraw_relaxing_walk.svg";
 
 function App() {
+  const [breeds, setBreeds] = useState([]);
+  const [selectedBreed, setSelectBreed] = useState("");
+  
+
+  useEffect(()=>{
+    fetch("https://dog.ceo/api/breeds/list/all")
+    .then(res=> res.json())
+    .then(res=> {
+      setBreeds(Object.keys(res.message))
+    })
+  },[])
 
   return (
     <div className="d-flex justify-content-center flex-column text-center">
@@ -9,7 +20,25 @@ function App() {
         <h1 className="mt-4 mb-5">Doggy Directory 🐶</h1>
       </header>
       <main role="main">
-    
+        <div className="d-flex justify-content-center">
+          <select
+            className="form-select w-25"
+            aria-label="Select a breed of dog to display results"
+            value={selectedBreed}
+            onChange={(e)=> setSelectBreed(e.target.value)}
+            >
+              <option value="">Select a breed</option>
+              {breeds.map(breed=>(
+                <option key={breed} value={breed}>{breed}</option>
+              ))}
+          </select>
+          <button 
+            className="btn btn-info mx-2"
+            disabled={!selectedBreed}
+            >
+            Search
+          </button>
+        </div>
       </main>
     </div>
   );
